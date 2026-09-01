acb5c1eaf3f26cc70f7acaa794b401c347daad7e
Mahyashahsavari <mahya.shahsavari431@gmail.com>
2026-09-01 14:14:04 +0330

    Update monosuite war room

--- apps/monosuite-war-room/src/main.tsx
+++ apps/monosuite-war-room/src/main.tsx
@@ -5,10 +5,10 @@ import { warRoomTheme } from '@monosuite/theme';
 import { MonosuiteProvider } from '@monosuite/ui';
 import '@monosuite/theme/styles.css';
 import { App } from './App';
-import { APP_UI_SCALE } from './shared/constants';
+import { initAppUiScale } from './shared/constants';
 import './app.css';
 
-document.getElementById('root')!.style.setProperty('--war-room-ui-scale', String(APP_UI_SCALE));
+initAppUiScale();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
